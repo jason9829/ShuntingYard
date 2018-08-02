0f56f6b8b6e9e42f642c734835854e6a80d8a72b
jason9829 <jason9829@live.com>
2018-08-02 21:44:47 +0800

    Arithmetic moved from ShuntingYard (module) to Arithmetic (module)

--- src/ShuntingYard.h
+++ src/ShuntingYard.h
@@ -1,14 +1,9 @@
 #ifndef _SHUNTINGYARD_H
 #define _SHUNTINGYARD_H
 
+#include "Token.h"
 #include "Stack.h"
-#include "TokenAffix.h"
-
 Token operateOnTokens(StackBlock *operatorStack, StackBlock *operandStack);
-Token *calculationOnTokens(Token *token_1, Token *token_2, Token *token_operator);
-Token *additionOf2Tokens(Token *token_1, Token *token_2);
-Token *subtractionOf2Tokens(Token *token_1, Token *token_2);
-Token *multiplicationOf2Tokens(Token *token_1, Token *token_2);
-Token *divisionOf2Tokens(Token *token_1, Token *token_2);
+
 
 #endif // _SHUNTINGYARD_H
